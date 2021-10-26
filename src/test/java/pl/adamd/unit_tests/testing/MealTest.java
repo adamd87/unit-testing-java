133c9d2d89c269320ca6781578007f13b6d98743
Adam <adam.drozdz1987@gmail.com>
2021-10-26 20:47:12 +0200

    Example with @Spy tag.

--- src/test/java/pl/adamd/unit_tests/testing/MealTest.java
+++ src/test/java/pl/adamd/unit_tests/testing/MealTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import pl.adamd.unit_tests.testing.extensions.IAExceptionIgnoreExtension;
 import pl.adamd.unit_tests.testing.order.Order;
 
@@ -24,7 +26,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
+
 class MealTest {
+
+    @Spy
+    private Meal mealSpy;
 
     @Test
     void shouldReturnDiscountedPrice() {
@@ -187,6 +193,23 @@ assertj-core:
         //then
         assertThat(result, equalTo(45));
     }
+
+    @Test
+    @ExtendWith(MockitoExtension.class)
+    void testMealSumPriceWithSpy(){
+        //given
+
+        given(mealSpy.getPrice()).willReturn(15);
+        given(mealSpy.getQuantity()).willReturn(3);
+
+        //when
+        int result = mealSpy.sumPrice();
+        //then
+//        then(meal).should().getPrice();
+//        then(meal).should().getQuantity();
+        assertThat(result, equalTo(45));
+    }
+
 
     private int calculatePrice(int price, int quantity) {
         return price * quantity;
